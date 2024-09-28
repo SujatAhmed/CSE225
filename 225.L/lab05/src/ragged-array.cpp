1451b89ed0bf0036f60915c488efb2e1e61975bb
Sujat Ahmed <sujatahmed842002@gmail.com>
2024-09-28 18:49:10 +0600

    fixed sum

--- 225.L/lab05/src/ragged-array.cpp
+++ 225.L/lab05/src/ragged-array.cpp
@@ -1,34 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() // 2-D ragged array
+int main() 
 {
-  int row;
-  int column;
-  cout << "Enter number of rows you want to enter: " << endl;
-  cin >> row;
-  cout << "Enter the number of columns you want to enter" << endl;
-  cin >> column;
-
-  int **ptr = new int *[row];
-  for (int i = 0; i < row; i++) {
-    ptr[i] = new int[column];
-  }
-  for (int i = 0; i < row; i++) {
-    for (int j = 0; j < column; j++) {
-      cout << "Row " << i + 1 << "Column " << j + 1 << endl;
-      cin >> ptr[i][j];
-    }
-  }
-
-  for (int i = 0; i < row; i++) {
-    for (int j = 0; j < column; j++) {
-      cout << ptr[i][j] << " ";
-    }
-    cout << endl;
-  }
-}
-void case2() { // 2D uniform array
   int row;
   cout << "Enter number of rows you want to enter: " << endl;
   cin >> row;
@@ -59,3 +33,4 @@ void case2() { // 2D uniform array
     delete[] ptr[i];
   }
 }
+
